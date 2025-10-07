c58c3b8f81a4fee50ad6f1d692c78e625d3c6dbc
Armando Villalba <armando_villalba@ande.corp>
2025-10-07 13:42:07 -0300

    compartir ok

--- lib/presentation/components/common/pad_viewer.dart
+++ lib/presentation/components/common/pad_viewer.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:share_plus/share_plus.dart';
 
 class PdfViewerScreen extends StatefulWidget {
   final File stringPdf;
@@ -14,8 +15,28 @@ class PdfViewerScreen extends StatefulWidget {
 class _PdfViewerScreenState extends State<PdfViewerScreen> {
   @override
   Widget build(BuildContext context) {
+    final params = ShareParams(
+    //  uri: widget.stringPdf.uri,
+     // text: 'Great picture',
+      files: [XFile(widget.stringPdf.path)],
+    );
+
+    void compartir() async {
+      final result = await SharePlus.instance.share(params);
+
+      if (result.status == ShareResultStatus.success) {
+        print('Thank you for sharing my website!');
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('PDF')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          compartir();
+        },
+        child: Icon(Icons.share),
+      ),
       body: PDFView(
         filePath: widget.stringPdf.path,
         enableSwipe: true,
