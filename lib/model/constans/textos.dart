67ad4f952956075877d8f33b141152e9c3f4418b
Armando Villalba <armando_villalba@ande.corp>
2025-10-21 09:52:06 -0300

    texto login

---
+++ lib/model/constans/textos.dart
@@ -0,0 +1,17 @@
+const estimadoCliente = '''
+  Estimado Cliente:\n
+  Desde ahora podés registrarte para Gestión Documental (EXPEDIENTES) y Gestión Comercial.\n
+  Para eso es necesario hacer click en el botón Regístrate, elegir el tipo de Trámite y seguir los pasos.\n
+  Con el trámite de Gestión Documental (EXPEDIENTES), podés autogestionar tus expedientes, es decir, presentar y realizar el seguimiento de tu expediente en forma sencilla y desde cualquier lugar sin necesidad de acercarte a la ANDE.\n
+  Con el trámite de Gestión Comercial, podés acceder a los siguientes servicios.\n
+      • Fraccionamiento a través de la WEB de tu cuenta.\n
+      • Facturar tu consumo del mes.\n
+      • Consultar y descargar las 15 últimas facturas emitidas.\n
+      • Acceder a gráficos comparativos de tu histórico de consumos e importes de los últimos 12 meses.\n
+      • Gestionar el envío de mensajes a tu celular, eligiendo las notificaciones que desees recibir.\n
+      • Bloquear consultas a datos de tu suministro.\n
+  Es importante aclarar que al registrarte al trámite de Gestión Comercial, ya tenés automáticamente acceso a la Gestión Documental (EXPEDIENTES).\n\n
+
+
+
+''';
--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form/model/constans/textos.dart';
 import 'package:form/presentation/components/drawer/custom_drawer.dart';
 import 'package:go_router/go_router.dart';
 import 'package:form/core/auth/auth_notifier.dart';
@@ -50,16 +51,16 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
           if (authData.state == AuthState.authenticated) {
             context.go('/'); // navegar solo cuando autenticado
           } else if (authData.state == AuthState.error) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Login fallido')),
-            );
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(const SnackBar(content: Text('Login fallido')));
           }
         },
         error: (err, stack) {
           if (!mounted) return;
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Error inesperado: $err')),
-          );
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('Error inesperado: $err')));
         },
         loading: () {},
       );
@@ -68,102 +69,144 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     return Scaffold(
       endDrawer: CustomDrawer(),
       appBar: AppBar(title: const Text("Mi Cuenta - Acceder")),
-      body: Stack(
-        children: [
-          Form(
-            key: loginFormKey,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  DropdownButtonFormField<DropdownItem>(
-                    value: selectedTipoDocumento,
-                    hint: const Text("Seleccionar Tipo de Documento"),
-                    items: dropDownItems
-                        .map((item) => DropdownMenuItem(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Form(
+              key: loginFormKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DropdownButtonFormField<DropdownItem>(
+                      value: selectedTipoDocumento,
+                      hint: const Text("Seleccionar Tipo de Documento"),
+                      items: dropDownItems
+                          .map(
+                            (item) => DropdownMenuItem(
                               value: item,
                               child: Text(item.name),
-                            ))
-                        .toList(),
-                    onChanged: isLoading
-                        ? null
-                        : (value) =>
-                            setState(() => selectedTipoDocumento = value),
-                    validator: (value) =>
-                        value == null ? 'Seleccione un tipo de documento' : null,
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: numeroController,
-                    decoration: const InputDecoration(
-                      labelText: 'Número de CI, RUC o Pasaporte',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Ingrese Número de CI, RUC o Pasaporte';
-                      }
-                      return null;
-                    },
-                    enabled: !isLoading,
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: passwordController,
-                    decoration:
-                        const InputDecoration(labelText: 'Contraseña'),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Ingrese Contraseña';
-                      }
-                      return null;
-                    },
-                    enabled: !isLoading,
-                  ),
-                  const SizedBox(height: 24),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: (isLoading || selectedTipoDocumento == null)
+                            ),
+                          )
+                          .toList(),
+                      onChanged: isLoading
                           ? null
-                          : () {
-                              if (loginFormKey.currentState!.validate()) {
-                                ref.read(authProvider.notifier).login(
-                                      numeroController.text.trim(),
-                                      passwordController.text.trim(),
-                                      selectedTipoDocumento!.id,
-                                      false,
-                                    );
-                              }
-                            },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50),
+                          : (value) =>
+                                setState(() => selectedTipoDocumento = value),
+                      validator: (value) => value == null
+                          ? 'Seleccione un tipo de documento'
+                          : null,
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: numeroController,
+                      decoration: const InputDecoration(
+                        labelText: 'Número de CI, RUC o Pasaporte',
                       ),
-                      child: const Text(
-                        'Acceder',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Ingrese Número de CI, RUC o Pasaporte';
+                        }
+                        return null;
+                      },
+                      enabled: !isLoading,
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Contraseña',
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Ingrese Contraseña';
+                        }
+                        return null;
+                      },
+                      enabled: !isLoading,
+                    ),
+                    const SizedBox(height: 24),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: (isLoading || selectedTipoDocumento == null)
+                            ? null
+                            : () {
+                                if (loginFormKey.currentState!.validate()) {
+                                  ref
+                                      .read(authProvider.notifier)
+                                      .login(
+                                        numeroController.text.trim(),
+                                        passwordController.text.trim(),
+                                        selectedTipoDocumento!.id,
+                                        false,
+                                      );
+                                }
+                              },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                        ),
+                        child: const Text(
+                          'Acceder',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
 
-          // Overlay de bloqueo
-          if (isLoading)
-            Container(
-              color: Colors.black.withOpacity(0.5),
-              child: const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.white,
-                  strokeWidth: 3,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            // Handle left button press
+                          },
+                          child: const Text('Olvido de Contraseña'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Handle right button press
+                          },
+                          child: const Text('Regístrate'),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        // color: Colors.red,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.primary,
+                          width: 2,
+                        ),
+                      ),
+                      child: Text(estimadoCliente,style: TextStyle(
+                        fontSize: 13
+                      ),),
+                    ),
+                  ],
                 ),
               ),
             ),
-        ],
+
+            // Overlay de bloqueo
+            if (isLoading)
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                child: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                    strokeWidth: 3,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
