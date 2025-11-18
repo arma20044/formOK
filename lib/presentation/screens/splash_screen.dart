2cd7bffe12ca4fb6f2e280068eab0671913f176b
Armando Villalba <armando_villalba@ande.corp>
2025-11-18 10:46:13 -0300

    ajuste splash

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,17 +1,59 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form/core/enviromens/enrivoment.dart';
 import 'package:form/provider/splash_init_provider.dart';
 import 'package:go_router/go_router.dart';
 
-class SplashScreen extends ConsumerWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Animación del logo
+   _controller = AnimationController(
+      duration: const Duration(milliseconds: 3000), // Duración de la animación
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.9,
+      end: 1.5,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut, // Curva suave sin rebote
+      ),
+    );
+
+     // 3. INICIAR LA ANIMACIÓN
+    _controller.forward(); 
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // Escucha el provider de inicialización
     ref.listen(splashInitProvider, (prev, next) {
       next.whenData((_) {
-        context.go('/'); // Home o ruta que corresponda
+        context.go('/'); // Ruta final luego del splash
       });
     });
 
@@ -20,13 +62,25 @@ class SplashScreen extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logoande.png', height: 50),
+            ScaleTransition(
+              scale: _scaleAnimation,
+              child: Image.asset(
+                'assets/images/logoande.png',
+                height: 80, // un poco más grande para apreciar el efecto
+              ),
+            ),
             const SizedBox(height: 20),
-            const CircularProgressIndicator(),
+         
+                  const SizedBox(height: 20),
+              
+                  Platform.isAndroid
+                      ? Text('Ver. ${Environment.appVersion.android.version}')
+                      : Text('Ver. ${Environment.appVersion.ios.version}'),
+             
+            
           ],
         ),
       ),
     );
   }
 }
-
